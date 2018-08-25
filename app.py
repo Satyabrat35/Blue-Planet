9ca04abbf00e73adcebd9f774c68635ecf588e75
Satyabrat35 <satyabrat35@gmail.com>
2018-08-25 20:02:29 +0530

    Must be the final

--- app.py
+++ app.py
@@ -49,6 +49,43 @@ def animals():
         #print("Error..")
         return render_template('home.html')
 
+@app.route('/species/<variable>')
+def save(variable):
+    name = variable.strip()
+    url = 'https://api.jsonbin.io/b/5b794f7a6376d24455a89004/9'
+    headers = {
+        'secret-key': '$2a$10$dKH7Mf31dIBNqbaH4Pcw4ucLNGMgr5ggMdBTBczssMZBsvNUyQePS'
+    }
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        resp = r.json()
+        data = resp['features']
+        
+        x = len(data)
+        y = 0
+        for i in range(0,x):
+            if name == data[i]['name']:
+                y = i
+                break
+        
+        title = data[y]['name']
+        facts = data[y]['facts']
+        iframe = data[y]['iframe']
+        status = data[y]['status']
+        scientific_name = data[y]['scientific name']
+        habitat = data[y]['habitat']
+        places = data[y]['places']
+        matter = data[y]['matter']
+        pic_1 = data[y]['pic_1']
+        pic_2 = data[y]['pic_2']
+        pic_3 = data[y]['pic_3']
+        
+        return render_template('index.html',title=title,facts=facts,iframe=iframe,status=status,scientific_name=scientific_name,habitat=habitat,places=places,matter=matter,pic_1=pic_1,pic_2=pic_2,pic_3=pic_3)
+    else:
+        #print("Error..")
+        return render_template('home.html')
+
+
  
 
 if __name__ == "__main__":
